b51f8a0006243bb2a8117a507d0618e2eff3d9e8
jaide2536 <sun_jamsod@hotmail.co.th>
2016-04-09 17:57:23 +0700

    # Edit 01 # เปลี่ยนสีตัวยานอวกาศเป็นสีขาว และเปลี่ยนรูปร่างยานอวกาศ

---
+++ SpaceShip.java
@@ -0,0 +1,33 @@
+package f2.spw;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class SpaceShip extends Sprite{
+
+	int step = 8;
+	
+	public SpaceShip(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(x-5, y+10, 10, 35);
+		g.fillRect(x-15, y+25, 30,5);
+		g.fillRect(x-25, y+40, 50,5);
+		
+	}
+
+	public void move(int direction){
+		x += (step * direction);
+		if(x < 0)
+			x = 0;
+		if(x > 400 - width)
+			x = 400 - width;
+	}
+
+
+}

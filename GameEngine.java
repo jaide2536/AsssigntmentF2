bebd36179a9f6ff341ca82a119aa3d16d0f51b0a
jaide2536 <sun_jamsod@hotmail.co.th>
2016-04-30 16:36:14 +0700

    # Edit 02 #, # Edit 06 #,# Edit 09 # # Edit 10 #,Extra เพิ่มยานพัธมิตร

--- GameEngine.java
+++ GameEngine.java
@@ -1,5 +1,6 @@
 package f2.spw;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -7,17 +8,20 @@ import java.awt.event.KeyListener;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Iterator;
-
 import javax.swing.Timer;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
 
 
 public class GameEngine implements KeyListener, GameReporter{
 	GamePanel gp;
 		
-	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();	
+	private ArrayList<Friend> friends = new ArrayList<Friend>();
 	private ArrayList<Bullet> bullets = new ArrayList<>();
-	private ArrayList<BulletNuke> bulletnukes = new ArrayList<>();		
-	private SpaceShip v;	
+	private ArrayList<BulletNuke> bulletnukes = new ArrayList<>();
+	private SpaceShip v;
+
 	
 	private Timer timer;
 	
@@ -50,10 +54,18 @@ public class GameEngine implements KeyListener, GameReporter{
 		gp.sprites.add(e);
 		enemies.add(e);
 	}
+
+	private void generateFriend(){
+		Friend a = new Friend((int)(Math.random()*390), 30);
+		gp.sprites.add(a);
+		friends.add(a);
+	}
+
 	
 	private void process(){
 		if(Math.random() < difficulty){
 			generateEnemy();
+			generateFriend();
 		}
 		
 		Iterator<Enemy> e_iter = enemies.iterator();
@@ -67,57 +79,79 @@ public class GameEngine implements KeyListener, GameReporter{
 				score += 100;
 			}
 		}
+		
+		Iterator<Friend> a_iter = friends.iterator();
+		while(a_iter.hasNext()){
+			Friend a = a_iter.next();
+			a.proceed();
+			
+			if(!a.isAlive()){
+				a_iter.remove();
+				gp.sprites.remove(a);
+				score += 100;
+			}
+		}
 
 		Iterator<Bullet> b_iter = bullets.iterator();
                 while(b_iter.hasNext()){
                     Bullet b = b_iter.next();
                     b.proceed();
                 }
-        }
 
         Iterator<BulletNuke> n_iter = bulletnukes.iterator();
                 while(n_iter.hasNext()){
                     BulletNuke n = n_iter.next();
                     n.proceed();
                 }
-		
+
 		gp.updateGameUI(this);
 		
 		Rectangle2D.Double vr = v.getRectangle();
 		Rectangle2D.Double er;
 		Rectangle2D.Double br;
 		Rectangle2D.Double nr;
+		Rectangle2D.Double ar;
 
 		for(Enemy e : enemies){
 			er = e.getRectangle();
 			if(er.intersects(vr)){
 				die();
 				return;
-
 			}
-		}
 
-		for(Bullet b : bullets ){
+
+			for(Bullet b : bullets ){
             br = b.getRectangle();
             if(br.intersects(er)){  
             	e.enemydie();
             	return;
         		}
 
-        for(BulletNuke n : bulletnukes ){
+        		for(Friend a : friends ){
+        			ar = a.getRectangle();
+        			if(br.intersects(ar)){
+            			a.frienddie();
+            			score -= 500;
+            			return;
+            		}
+            	}
+			}
+
+			for(BulletNuke n : bulletnukes ){
             	nr = n.getRectangle();
             	if(nr.intersects(er)){  
             	e.enemydie();
             	return;
         		}
         	}
+		}
+	}
 
 
 	public void fire(){
 		Bullet b = new Bullet((v.x)+12,(v.y)-20);
 		gp.sprites.add(b);
 		bullets.add(b);
-		}	
 	}
 
 	public void firenu(){
@@ -141,17 +175,15 @@ public class GameEngine implements KeyListener, GameReporter{
 		case KeyEvent.VK_RIGHT:
 			v.move(1);
 			break;
+			
 		case KeyEvent.VK_D:
 			difficulty += 0.1;
 			break;
-		case KeyEvent.VK_DOWN:
-			v.move(0,1);
-			break;
-		case KeyEvent.VK_UP:
-			v.move(0,-1);
-			break;
 		case KeyEvent.VK_A:
 			fire();
+			break;
+		case KeyEvent.VK_N:
+			firenu();
 			break;
 		}
 	}
